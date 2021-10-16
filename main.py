4fd18b02f9f3452c80e266f3de0e8ce1f9e9794c
MaiconCunhaVieira <maicon_c_v@hotmail.com>
2021-10-16 19:06:49 -0300

    getting headers from dataset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import csv
-from typing import List
+from typing import List, Tuple
 from datainstance import DataInstance
 
 
@@ -12,17 +12,17 @@ def get_file_name() -> str:
         sys.exit()
 
 
-def read_dataset(file_name: str) -> List[DataInstance]:
+def read_dataset(file_name: str) -> Tuple[List[DataInstance], List[str]]:
     data_instances = []
     try:
-        with open('dataset/' + file_name) as data_file:
+        with open(file_name) as data_file:
             lines = csv.reader(data_file, delimiter=';')
-            next(lines)  # skip headers line
+            headers = next(lines)
             for line in lines:
                 target = line[-1]  # get last column
                 attributes = line[:-1]  # get all but last column
                 data_instances.append(DataInstance(attributes, target))
-        return data_instances
+        return data_instances, headers
     except FileNotFoundError:
         print('Dataset file not found. Exiting...')
         sys.exit()
@@ -30,7 +30,7 @@ def read_dataset(file_name: str) -> List[DataInstance]:
 
 def main():
     dataset_file_name = get_file_name()
-    data_instances = read_dataset(dataset_file_name)
+    data_instances, headers = read_dataset(dataset_file_name)
 
 
 if __name__ == '__main__':
