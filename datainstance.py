e54ae1dec0a884b3b57fa6bddc4cfb1d7c818f5c
azhow <arthur.zachow@gmail.com>
2021-10-17 00:00:45 +0000

    Created attribute class to stop using indexes and updated read_dataset

--- datainstance.py
+++ datainstance.py
@@ -1,8 +1,31 @@
 from typing import List
 
+class Attribute(object):
+    def __init__(self, name: str, value: str, attr_type: str):
+        self.name = name
+        self.attr_type = attr_type
+        if (self.attr_type == 'c'):
+            self.value = value
+        elif self.attr_type == 'n':
+            self.value = float(value)
+        else:
+            raise Exception("Invalid metadata type")
 
+    
+    def __str__(self) -> str:
+        return "Attribute(" \
+            "name=" + self.name + \
+            ", value=" + str(self.value) + \
+            ", type=" + self.attr_type + \
+            "}" 
+
+    
+    def __repr__(self) -> str:
+        return str(self)
+
+        
 class DataInstance:
-    def __init__(self, attributes: List[str], target: str):
+    def __init__(self, attributes: List[Attribute], target: str):
         self.attributes = attributes
         self.target = target
 
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 import csv
 from typing import List, Tuple
-from .datainstance import DataInstance
+from pathlib import Path
+from .datainstance import DataInstance, Attribute
 
 
 def get_file_name() -> str:
@@ -12,15 +13,50 @@ def get_file_name() -> str:
         sys.exit()
 
 
+def read_metadata(file_path: str) -> List[str]:
+    """
+    Reads the metadata file associated with data.
+    Metadata files are files with only one csv line.
+    It is expected that metadata file names are just the db file name with _metadata appended to it.
+    """
+    try:
+        p = Path(file_path)
+        p = p.with_name(p.stem + "_metadata.csv")
+        with open(p) as f:
+            reader = csv.reader(f, delimiter=",")
+            metadata = next(reader)
+
+        return metadata
+    except FileNotFoundError:
+        print("Dataset metadata file not found. Exiting...")
+        sys.exit()
+
+
+def create_attributes(header: List[str], attribute_values: List[str], attributes_metadata: List[str]) -> List[Attribute]:
+    """
+    Create a list of attribute objects based on the header, values and type metadata.
+    """
+    attributes = []
+
+    # For each attribute
+    for idx in range(len(header)):
+        attributes.append(Attribute(header[idx], attribute_values[idx], attributes_metadata[idx]))
+
+    return attributes
+
+
 def read_dataset(file_name: str) -> Tuple[List[DataInstance], List[str]]:
     data_instances = []
+    metadata = read_metadata(file_name)
+
     try:
         with open(file_name) as data_file:
             lines = csv.reader(data_file, delimiter=';')
             headers = next(lines)
             for line in lines:
                 target = line[-1]  # get last column
-                attributes = line[:-1]  # get all but last column
+                attribute_values = line[:-1]  # get all but last column
+                attributes = create_attributes(headers[:-1], attribute_values, metadata)
                 data_instances.append(DataInstance(attributes, target))
         return data_instances, headers
     except FileNotFoundError:
