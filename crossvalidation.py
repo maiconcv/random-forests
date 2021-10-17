a2c1c6d22e22b5644b1d4a6976bb8b12bd3b283e
azhow <arthur.zachow@gmail.com>
2021-10-17 21:30:44 +0000

    Fixed typo on type hint and created new metadata file

--- crossvalidation.py
+++ crossvalidation.py
@@ -1,15 +1,17 @@
 import random
 from itertools import cycle
+from typing import List
+from datainstance import DataInstance
 
 
-def cross_validation_division(dataset, k_folds, r_repetitions, seed=None):
+def cross_validation_division(
+    dataset: List[DataInstance], k_folds: int, r_repetitions: int, seed=None):
     repetitions = []
-    for repetition in range(0, r_repetitions):
+    for repetition in range(r_repetitions):
         if repetition == 0:
             repetition_offset = 0
         else:
             repetition_offset = get_repetition_offset(dataset, k_folds, seed, repetition)
-        print(repetition_offset)
         repetitions.append(fold_division(dataset, k_folds, repetition_offset))
     return repetitions
 
@@ -29,20 +31,20 @@ def fold_division(dataset, k_folds, repetition_offset):
     folds = []
 
     # shift N times, so that folds are different between repetitions
-    for i in range(0, repetition_offset):
+    for i in range(repetition_offset):
         next(circular_iterator)
 
     # create k folds
-    for ith_fold in range(0, k_folds):
+    for ith_fold in range(k_folds):
         fold = []
-        for ith_element in range(0, fold_size):
+        for ith_element in range(fold_size):
             fold.append(next(circular_iterator))
         folds.append(fold)
 
     # add remaining instances to last fold when num_instances/k_folds is non-integer,
     # to not lose instances on fold division
     num_remaining_instances = num_instances - (k_folds * fold_size)
-    for i in range(0, num_remaining_instances):
+    for i in range(num_remaining_instances):
         folds[-1].append(next(circular_iterator))
 
     return folds
